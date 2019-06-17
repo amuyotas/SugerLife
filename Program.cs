0b6edb8d53bf576ad96b95f0de9bdf3a266f6c68
ysato <ysato@mwj-warranty.jp>
2019-06-17 15:51:08 +0900

    3rd commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace _190617_temp_vsc
 {
     class Program
     {
-        static void Hell(string s) => Console.WriteLine($"Hello {s}");
+        static void Hello(string s) => Console.WriteLine($"Hello {s}");
 
         static void Main(string[] args) => Hello("World.");
     }
