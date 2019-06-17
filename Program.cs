639d8b19b610a9dc52af8bfdc0bae2794f23cc59
ysato <ysato@mwj-warranty.jp>
2019-06-17 15:46:52 +0900

    1st commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _190617_temp_vsc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
